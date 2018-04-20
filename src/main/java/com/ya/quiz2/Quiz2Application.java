973c87f74586a7b6e0fa8b9381ae73652a9c17fb
Kaer-Tangs <574884549@qq.com>
2018-04-20 16:10:32 +0800

    Initial commit

---
+++ src/main/java/com/ya/quiz2/Quiz2Application.java
@@ -0,0 +1,12 @@
+package com.newer.quiz2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Quiz2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Quiz2Application.class, args);
+	}
+}
